4f7b9f60e4a7f216081f93246867a98fe6357627
Yulisa Ramírez Rodríguez <s20120218@alumnos.itsur.edu.mx>
2023-09-06 22:42:17 -0600

    App DiceRoller

---
+++ DiceRoller_App/app/src/main/java/com/yulyramirez/diceroller/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.yulyramirez.diceroller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.yulyramirez.diceroller.ui.theme.DiceRollerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DiceRollerTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    DiceRollerApp()
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DiceRollerApp(){
+    DiceWithButtonAndImage(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center))
+}
+
+private fun getRandomDiceImage() : Int{
+    return (1..6).random()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier){
+    var res by remember { mutableStateOf( 1) }
+    var mut by remember { mutableStateOf( 1) }
+    val imageResource = when(res) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+    val imageResource1 = when(mut) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally)
+    {
+        Image(painter = painterResource(imageResource), contentDescription = res.toString(), )
+        Image(painter = painterResource(imageResource1), contentDescription = mut.toString())
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            res = getRandomDiceImage()
+            mut = getRandomDiceImage()
+        }) {
+            Text(stringResource(R.string.roll), fontSize = 24.sp)
+        }
+    }
+
+}
